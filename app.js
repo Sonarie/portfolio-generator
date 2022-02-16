5ac4432c67da8658b0352d8e68a6c52e557a4d67
Sonarie <sonarie1980@gmail.com>
2022-02-15 18:46:20 -0600

    moving my previous work

---
+++ app.js
@@ -0,0 +1,10 @@
+const printProfileData = (profileDataArr) => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log("================");
+
+  profileDataArr.forEach((profileItem) => console.log(profileItem));
+};
