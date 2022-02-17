40cc6ff52f87261ef94e0be85c80579d424b2553
Sonarie <sonarie1980@gmail.com>
2022-02-16 21:38:55 -0600

    new update

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const printProfileData = (profileDataArr) => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-  console.log("================");
+const profileDataArgs = process.argv.slice(2);
 
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
